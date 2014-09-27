85d99cd1c9cba64dde3cb83cbb36f376d4cb1492
Richard D. Morey <richarddmorey@gmail.com>
2014-09-27 23:21:43 +0200

    initial commit

---
+++ BayesFactorExtras/R/BayesFactorExtras-package.R
@@ -0,0 +1,28 @@
+
+
+#' Extra functions for use with the BayesFactor package
+#'
+#' This package contains extra functions that can be used to enhance the BayesFactor
+#' package, such as plots and reports.
+#'
+#'\tabular{ll}{ Package: \tab BayesFactorExtras\cr Type: \tab Package\cr Version: \tab
+#'0.1.0\cr Date: \tab 2014-11-22\cr License: \tab GPL 2.0\cr LazyLoad: \tab 
+#'yes\cr }
+#'
+#'@title The BayesFactorExtras package
+#'@name BayesFactorExtras-package
+#'@aliases BayesFactorExtras-package BayesFactorExtras
+#'@docType package
+#'@author Richard D. Morey
+#'  
+#'  Maintainer: Richard D. Morey <richarddmorey@@gmail.com>
+#'@seealso \code{\link[BayesFactor:BayesFactor-package]{BayesFactor}}
+#'@keywords BayesFactor
+#'@examples
+#'
+#'## See specific functions for examples.
+#'
+NULL
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 BayesFactorExtras
 =================
+
+`BayesFactorExtras` is an R package which contains extra features related to the `BayesFactor` package, such as plots and analysis reports.
