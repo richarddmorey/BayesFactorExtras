bd901d7c094a867aa2c82caa86244ce5e7b120f0
Felix Schönbrodt <felix@nicebread.de>
2015-01-22 10:07:47 +0100

    Added seqBF and seqBFplot functions
    - fixed a lot of R CMD check complaints

--- BayesFactorExtras/R/seqBF.R
+++ BayesFactorExtras/R/seqBF.R
@@ -2,8 +2,15 @@
 #' @details Compute a sequential Bayes factor object
 #' @param BFobject A BayesFactor object
 #' @param ... User parameters which override the settings of the original \code{BFobject}
-#' @param min.n Smallest number of participants for which the first BF should be computed. In a one-sample t test / paired  test, this is the overall sample size. In two-sample t tests and ANOVAs, this is the minimum size for each group.#
+#' @param min.n Smallest number of participants for which the first BF should be computed. In a one-sample t test / paired  test, this is the overall sample size. In two-sample t tests and ANOVAs, this is the minimum size for each group. #
+#' @param var.id The column that defines which rows belong to one person/object of observation. This is relevant for repeated measurements, as we want to sample complete observations. If this parameter is left as NULL, the function tries to guess the relevant identifying variable.
 #' @param step Step size with which the number of participants is increased
+#' @param verbose Print diagnostic information?
+
+## Not implemented yet:
+# @param sampling "as.is": Sample consecutively from the data, in the order the data dropped in. This can be done with any BF, but is problematic if for example the first 20 observations are from the experimental group only (then the BF cannot be computed). "balanced": Sample from each level of a factor. Only works for t-tests so far.
+
+
 #' @author Felix Schönbrodt
 
 #' @export
@@ -22,69 +29,77 @@
 #' plot(anova1.seq)
 #'
 #' data(puzzles)
-#' #a2 <- anovaBF(RT ~ shape + color, data = puzzles, whichRandom = "ID", progress=FALSE)
-#' #a2.seq <- seqBF(a2)
-#'	
-#' # Two-sample t test, one-sided test
-#' ttest.2 <- ttestBF(x = sleep$extra[sleep$group==1], y = sleep$extra[sleep$group==2], 
-#'					  nullInterval=c(-Inf, 0))
-#' ttest.2.seq <- seqBF(ttest.2, min.n=5)
-#' plot(ttest.2.seq)
-#' 
+#' (anova2 <- anovaBF(RT ~ shape*color + ID, data = puzzles, whichRandom = "ID", progress=FALSE))
+#' (anova2.seq <- seqBF(anova2, min.n=5))
+#' plot(anova2.seq)
 
-seqBF <- function(BFobject, min.n=10, step=1, ...) {
-	#if (class(BFobject) != "BFBayesFactor") stop(paste0("seqBF does not work yet for objects of class ", class(BFobject)))
 
-	dat <- BFobject@data
+seqBF <- function(BFobject, min.n=10, step=1, var.id=NULL, verbose=TRUE, ...) {
+
+	dat <- BFobject@data	# stores the full data set
 	BFtype <- class(BFobject@numerator[[1]])
+	random <- FALSE	# flag whether data has random factors or not.
 
-	n.max <- NULL
-	switch(BFtype,
-		BFoneSample = {
-			max.n = nrow(dat)
-		},
-		BFindepSample = {
-			count = dat %>% count(group)
-			max.n = max(count$n)
-		},
-		BFlinearModel = {
-			# differentiate between anovaBF and lmBF: get RHS of denominator formula; ==1: lmBF; !=1: anovaBF
-			if (length(all.vars(as.formula(BFobject@denominator@identifier$formula))) == 1) {
-				# lmBF
-				stop("seqBF does not work for lmBF() yet.")
-			} else {
-				# anovaBF
-				call <- paste0("group_by(dat, ", paste(names(which(BFobject@numerator[[1]]@dataTypes == "fixed")), collapse=", "), ")")
-				dat_grouped <- eval(parse(text=call))
-				count = dat_grouped %>% summarise(n=n())
-				max.n = max(count$n)
+	# Determine the number of observations if var.id is given by user
+	if (!is.null(var.id)) {
+		count = dat %>% group_by_(var.id) %>% summarise(n=n())
+		max.n = nrow(count)
+	}
+	
+	# var.id is NULL? Try to guess the grouping variable
+	if (is.null(var.id)) {
+		if (verbose==TRUE) print("Trying to guess the grouping variable ...")
+		switch(BFtype,
+			
+			# one sample t-test: each row is one observation
+			BFoneSample = {
+				max.n = nrow(dat)
+				if (verbose==TRUE) print("No grouping factor detected.")
+				},
+			BFindepSample = {
+				max.n = nrow(dat)
+				if (verbose==TRUE) print("No grouping factor detected.")
+				},
+			BFlinearModel = {
+				# differentiate between anovaBF and lmBF: get RHS of denominator formula; ==1: lmBF; !=1: anovaBF
+				if (length(all.vars(as.formula(BFobject@denominator@identifier$formula))) == 1) {
+					# no random factor / no repeated measurement
+					max.n = nrow(dat)
+					if (verbose==TRUE) print("No grouping factor detected.")
+				} else {
+					# repeated measurement
+					# TODO: This works if only one random factor is given (e.g., subject ID in a repeated measurement). Generalize to multiple random factors
+					var.id <- all.vars(as.formula(BFobject@denominator@identifier$formula))[2]
+					count = dat %>% group_by_(var.id) %>% summarise(n=n())
+					max.n = nrow(count)
+					random = TRUE
+					if (verbose==TRUE) print(paste0("Grouping factor detected: ", var.id))
+				}
 			}
-		}
-	)
+		)
+	}
+
 	# at which n's should the BF be calculated?
 	ns <- seq(min.n, max.n, by=step)
 	if (tail(ns, 1) != max.n) ns <- c(ns, max.n)
 
-	pb <- txtProgressBar(min = 0, max = length(ns), style=3)
+	if (verbose==TRUE) pb <- txtProgressBar(min = 0, max = length(ns), style=3)
 	resSeq <- data.frame()
 	# ---------------------------------------------------------------------
-	# Do the sequential calculation of BFs
+	# Do the sequential calculation of BFs. Each design needs it's own sampling scheme
 	for (i in 1:length(ns)) {
-		switch(BFtype,
-			BFoneSample = {BFobject@data <- data.frame(y=dat$y[1:ns[i]])},
-			BFindepSample = {
-				# sample min.n from each group
-				BFobject@data <- dat %>% group_by(group) %>% slice(1:ns[i]) %>% ungroup() %>% as.data.frame()
-				},
-			BFindepSample = {
-				# sample min.n from each group
-				BFobject@data <- dat %>% group_by(group) %>% slice(1:ns[i]) %>% ungroup() %>% as.data.frame()
-				},
-			BFlinearModel = {
-				# sample min.n from each cell (also in interaction designs)
-				BFobject@data <- dat_grouped %>% slice(1:ns[i]) %>% ungroup() %>% as.data.frame()
-				}
-		)
+		
+		if (random == FALSE) {
+			BFobject@data <- dat[1:ns[i], , drop=FALSE]
+		}
+		
+		if (random == TRUE) {
+			var.id.pos <- NULL	# hack to please CRAN
+			dat$var.id.pos <- as.numeric(dat[, var.id])
+			BFobject@data <- dat %>% filter(var.id.pos <= ns[i]) %>% select(-var.id.pos) %>% as.data.frame()
+		}
+		
+		
 		# Get rid of unused factor levels
 		for (v in 1:ncol(BFobject@data)) {
 			if (is.factor(BFobject@data[, v])) {
@@ -96,6 +111,8 @@ seqBF <- function(BFobject, min.n=10, step=1, ...) {
 		  BFobject@numerator[[v]]@analysis = list()
 		}
 		BFobject@denominator@analysis = list()
+		
+		# recompute Bayes factor on reduced data set
 		res1 <- recompute(BFobject, progress=FALSE)
 		resSeq <- rbind(resSeq, data.frame(
 			n  = ns[i], 
@@ -103,11 +120,13 @@ seqBF <- function(BFobject, min.n=10, step=1, ...) {
 			bf = res1@bayesFactor$bf,			
 			error = res1@bayesFactor$error
 		))
-		setTxtProgressBar(pb, i)
+		if (verbose==TRUE) setTxtProgressBar(pb, i)
 	}
-	close(pb)
+	if (verbose==TRUE) close(pb)
+
 	# Define a new final BF-object. If no user args are provided, this is identical to the original BFobject
 	BFobject.new <- res1
+	
 	# define a new S4 class. You have to set the 'where' argument to please R CMD check: https://stat.ethz.ch/pipermail/r-devel/2007-April/045475.html
 	setClass("BFBayesFactorSeq", slots = c(bayesFactorSeq="data.frame"), contains = class(BFobject), where=topenv(parent.frame()))
 
@@ -123,13 +142,74 @@ seqBF <- function(BFobject, min.n=10, step=1, ...) {
 }
 
 
+
 #' @export
 plot.BFBayesFactorSeq <- function(x, ...) {
+	dat <- x@bayesFactorSeq
+	dat <- na.omit(dat)
 	if (max(x@bayesFactorSeq$Alt) == 1) {
-		p1 <- seqBFplot(n = x@bayesFactorSeq$n, bf = x@bayesFactorSeq$bf, log.it=FALSE)
+		p1 <- seqBFplot(n = dat$n, bf = dat$bf, log.it=FALSE)
 	} else {
-		p1 <- seqBFplot(n = x@bayesFactorSeq$n, bf = x@bayesFactorSeq$bf, linetype=factor(x@bayesFactorSeq$Alt, labels=names(x@numerator)), log.it=FALSE)
-		p1 <- p1 + scale_linetype_discrete("Alternative")
+		p1 <- seqBFplot(n = dat$n, bf = dat$bf, linetype=factor(dat$Alt, labels=names(x@numerator)), linetype.label="Alternative", log.it=FALSE)
 	}
 	return(p1)
 }
+
+
+
+
+
+## ======================================================================
+## SPEICHER
+## ======================================================================
+
+# switch(BFtype,
+# 	BFoneSample = {
+# 		max.n = nrow(dat)
+# 	},
+# 	BFindepSample = {
+# 		count = dat %>% count(group)
+# 		max.n = max(count$n)
+# 	},
+# 	BFlinearModel = {
+# 		# differentiate between anovaBF and lmBF: get RHS of denominator formula; ==1: lmBF; !=1: anovaBF
+# 		if (length(all.vars(as.formula(BFobject@denominator@identifier$formula))) == 1) {
+# 			# lmBF
+# 			stop("seqBF does not work for lmBF() yet.")
+# 		} else {
+# 			# anovaBF
+# 			n.models <- length(BFobject@numerator)
+# 			call <- paste0("group_by(dat, ", paste(names(which(BFobject@numerator[[n.models]]@dataTypes == "fixed")), collapse=", "), ")")
+# 			dat_grouped <- eval(parse(text=call))
+# 			count = dat_grouped %>% summarise(n=n())
+# 			max.n = max(count$n)	# largest cell
+# 		}
+# 	}
+# )
+
+
+
+
+# switch(BFtype,
+# 	# one sample t test
+# 	BFoneSample = {BFobject@data <- data.frame(y=dat$y[1:ns[i]])},
+#
+# 	# two sample t test
+# 	BFindepSample = {
+# 		# sample min.n from each group
+# 		BFobject@data <- dat %>% group_by(group) %>% slice(1:ns[i]) %>% ungroup() %>% as.data.frame()
+# 		},
+# 	BFlinearModel = {
+# 		# sample min.n from each cell (also in interaction designs)
+# 		BFobject@data <- dat_grouped %>% slice(1:ns[i]) %>% ungroup() %>% as.data.frame()
+# 		}
+# )
+
+
+
+
+# @export
+# show.BFBayesFactorSeq <- function(x, ...) {
+# 	print("Sequential BayesFactor object. The final Bayes factor is:\n=========================")
+# 	print(x)
+# }
--- BayesFactorExtras/R/seqBFplot.R
+++ BayesFactorExtras/R/seqBFplot.R
@@ -7,11 +7,14 @@
 #' @param n A vector of numbers for the x axis
 #' @param bf A vector of Bayes factors (same length as x)
 #' @param linetype If several lines should be drawn, this variable defines which data point belongs to which line
+#' @param linetype.label The heading of the linetype legend
 #' @param xlab Label for x axis
 #' @param ylab Label for y axis
+#' @param ylim Limits of y axis, in raw BF units. If NA, plot is automatically adjusted to range of data.
 #' @param main Main title
 #' @param log.it Should the Bayes factor in the \code{bf} parameter be logged?
-#' @param forH1 If \code{TRUE}, positive bfs mean evidence in favor of H1 ("H1 over H0" Bayes factor). This is the default in the Bayes factor package.
+#' @param forH1 If \code{TRUE}, positive BFs mean evidence in favor of H1 ("H1 over H0" Bayes factor). This is the default in the Bayes factor package.
+#' @param fontsize Controls the font size of the annotation
 #'
 #' @export
 #' @import ggplot2
@@ -36,21 +39,50 @@
 #' seqBFplot(5:10, bf)
 #' seqBFplot(1:100, cumsum(rnorm(100, 2)))
 
-seqBFplot <- function(n, bf, linetype=NA, xlab="n", ylab="log(bf)", main="", log.it=TRUE, forH1=TRUE) {
+seqBFplot <- function(n, bf, linetype=NA, linetype.label="Alternative", xlab="n", ylab="log(BF)", main="", ylim=NA, log.it=TRUE, forH1=TRUE, fontsize=3.2) {
 	if (length(n) != length(bf)) stop("`n` and `bf` should have the same length")
 	if (length(n) < 1) stop("`n`and `bf` must habe length > 1")
 		
 	if (log.it==TRUE) bf <- log(bf)
 		
+	# automatically set ylim
+	y_breaks <- c(c(-log(c(100, 30, 10, 3)), 0, log(c(3, 10, 30, 100))))
+	if (is.na(ylim[1])) {
+		# find out in which y_break interval the lowest and highest bf fall
+		min_boundary_x <- as.numeric(cut(min(bf)*1.02, c(-Inf, y_breaks, Inf)))-1
+		max_boundary_x <- as.numeric(cut(max(bf)*1.02, c(-Inf, y_breaks, Inf)))
+		
+		if (min_boundary_x == 0 | min_boundary_x > length(y_breaks)) {
+			min_boundary <- min(bf)*1.02
+		} else {
+			min_boundary <- y_breaks[min_boundary_x]
+		}
+		if (max_boundary_x > length(y_breaks)) {
+			max_boundary <- max(bf)*1.02
+		} else {
+			max_boundary <- y_breaks[max_boundary_x]
+		}
+		
+		# ... but always show at least "anecdotal" in each direction
+		if (min_boundary > log(1/3)) min_boundary <- log(1/3)
+		if (min_boundary > min(bf)) min_boundary <- min(bf)*1.02
+		if (max_boundary < log(3)) max_boundary <- log(3)
+		if (max_boundary < max(bf)) max_boundary <- max(bf)*1.02
+		ylim <- c(min_boundary, max_boundary)
+	} else {
+		ylim <- log(ylim)
+	}
+		
+		
 	df <- data.frame(n, bf, Alt=factor(linetype))
-	p1 <- ggplot(df, aes(x=n, y=bf)) + theme_bw() + ylab(ylab) + xlab(xlab)
+	p1 <- ggplot(df, aes(x=n, y=bf)) + theme_bw() + ylab(ylab) + xlab(xlab)# + theme(axis.text.x = element_text(size=unit(fontsize, "points")), axis.text.y = element_text(size=unit(fontsize, "points"))) 
 	
 	# more than one data point? Line plot
 	if (length(n) > 1) {
 		if (is.na(linetype[1])) {
 			p1 <- p1 + geom_line()
 		} else {
-			p1 <- p1 + geom_line(aes(linetype=Alt, group=Alt))
+			p1 <- p1 + geom_line(aes(linetype=Alt, group=Alt)) + scale_linetype_discrete(linetype.label) + theme(legend.position = "top", legend.direction="vertical")
 		}
 		
 	
@@ -73,48 +105,79 @@ seqBFplot <- function(n, bf, linetype=NA, xlab="n", ylab="log(bf)", main="", log
 		x.breaks <- n
 	}
 	
-	p1 <- p1 + scale_x_continuous(breaks=x.breaks)
+	p1 <- p1 + scale_x_continuous(breaks=x.breaks) + coord_cartesian(ylim=ylim)
 	
 	
 	# All the annotation stuff ...
-	p1 <- p1 + geom_hline(yintercept=c(c(-log(c(100, 30, 10, 3)), log(c(3, 10, 30, 100)))), linetype="dotted", color="darkgrey")
+	hlines <- c(-log(c(100, 30, 10, 3)), log(c(3, 10, 30, 100)))
+	hlines_sel <- hlines[which(hlines > min(ylim) & hlines < max(ylim))]
+	p1 <- p1 + geom_hline(yintercept=hlines_sel, linetype="dotted", color="darkgrey")
 	p1 <- p1 + geom_hline(yintercept=log(1), linetype="dashed", color="grey20")
 
-	p1 <- p1 + annotate("text", x=max(n), y=-5.15, label=paste0("~~Extreme~H[", ifelse(forH1==TRUE,0,1), "]"), 
-		hjust=1, vjust=.5, size=3.2, color="black", parse=TRUE)
-	p1 <- p1 + annotate("text", x=max(n), y=-4.00, label=paste0("~~Very~strong~H[", ifelse(forH1==TRUE,0,1), "]"), 
-		hjust=1, vjust=.5, size=3.2, color="black", parse=TRUE)
-	p1 <- p1 + annotate("text", x=max(n), y=-2.85, label=paste0("~~Strong~H[", ifelse(forH1==TRUE,0,1), "]"), 
-		hjust=1, vjust=.5, size=3.2, color="black", parse=TRUE)
-	p1 <- p1 + annotate("text", x=max(n), y=-1.7 , label=paste0("~~Moderate~H[", ifelse(forH1==TRUE,0,1), "]"), 
-		hjust=1, vjust=.5, size=3.2, color="black", parse=TRUE)
-	p1 <- p1 + annotate("text", x=max(n), y=-.55 , label=paste0("~~Anectodal~H[", ifelse(forH1==TRUE,0,1), "]"), 
-		hjust=1, vjust=.5, size=3.2, color="black", parse=TRUE)
+	if (min(ylim)<= -5.15) {
+		p1 <- p1 + annotate("text", x=Inf, y=-5.15, label=paste0("~~Extreme~H[", ifelse(forH1==TRUE,0,1), "]"), 
+		hjust=0, vjust=.5, size=fontsize, color="black", parse=TRUE)
+	}
+	
+	if (min(ylim)<= -4) {
+	p1 <- p1 + annotate("text", x=Inf, y=-4.00, label=paste0("~~Very~strong~H[", ifelse(forH1==TRUE,0,1), "]"), 
+		hjust=0, vjust=.5, size=fontsize, color="black", parse=TRUE)
+	}
+		
+	if (min(ylim)<= -2.85) {	
+	p1 <- p1 + annotate("text", x=Inf, y=-2.85, label=paste0("~~Strong~H[", ifelse(forH1==TRUE,0,1), "]"), 
+		hjust=0, vjust=.5, size=fontsize, color="black", parse=TRUE)
+	}
+		
+	if (min(ylim)<= -1.7) {	
+	p1 <- p1 + annotate("text", x=Inf, y=-1.7 , label=paste0("~~Moderate~H[", ifelse(forH1==TRUE,0,1), "]"), 
+		hjust=0, vjust=.5, size=fontsize, color="black", parse=TRUE)
+	}
+		
+	p1 <- p1 + annotate("text", x=Inf, y=-.55 , label=paste0("~~Anectodal~H[", ifelse(forH1==TRUE,0,1), "]"), 
+		hjust=0, vjust=.5, size=fontsize, color="black", parse=TRUE)
+	p1 <- p1 + annotate("text", x=Inf, y=.55  , label=paste0("~~Anectodal~H[", ifelse(forH1==TRUE,1,0), "]"), 
+		hjust=0, vjust=.5, vjust=.5, size=fontsize, color="black", parse=TRUE)
 
-	p1 <- p1 + annotate("text", x=max(n), y=5.15, label=paste0("~~Extreme~H[", ifelse(forH1==TRUE,1,0), "]"), 
-		hjust=1, vjust=.5, size=3.2, color="black", parse=TRUE)
-	p1 <- p1 + annotate("text", x=max(n), y=4.00, label=paste0("~~Very~strong~H[", ifelse(forH1==TRUE,1,0), "]"), 
-		hjust=1, vjust=.5, size=3.2, color="black", parse=TRUE)
-	p1 <- p1 + annotate("text", x=max(n), y=2.86 , label=paste0("~~Strong~H[", ifelse(forH1==TRUE,1,0), "]"), 
-		hjust=1, vjust=.5, size=3.2, color="black", parse=TRUE)
-	p1 <- p1 + annotate("text", x=max(n), y=1.7  , label=paste0("~~Moderate~H[", ifelse(forH1==TRUE,1,0), "]"), 
-		hjust=1, vjust=.5, size=3.2, color="black", parse=TRUE)
-	p1 <- p1 + annotate("text", x=max(n), y=.55  , label=paste0("~~Anectodal~H[", ifelse(forH1==TRUE,1,0), "]"), 
-		hjust=1, vjust=.5, vjust=.5, size=3.2, color="black", parse=TRUE)
+	if (max(ylim) >= 5.15) {
+		p1 <- p1 + annotate("text", x=Inf, y=5.15, label=paste0("~~Extreme~H[", ifelse(forH1==TRUE,1,0), "]"), 
+			hjust=0, vjust=.5, size=fontsize, color="black", parse=TRUE)
+	}
+	if (max(ylim) >= 4) {
+		p1 <- p1 + annotate("text", x=Inf, y=4.00, label=paste0("~~Very~strong~H[", ifelse(forH1==TRUE,1,0), "]"), 
+			hjust=0, vjust=.5, size=fontsize, color="black", parse=TRUE)
+	}
+	if (max(ylim) >= 2.86) {
+		p1 <- p1 + annotate("text", x=Inf, y=2.86 , label=paste0("~~Strong~H[", ifelse(forH1==TRUE,1,0), "]"), 
+		hjust=0, vjust=.5, size=fontsize, color="black", parse=TRUE)
+	}
+	if (max(ylim) >= 1.7) {
+	p1 <- p1 + annotate("text", x=Inf, y=1.7  , label=paste0("~~Moderate~H[", ifelse(forH1==TRUE,1,0), "]"), 
+		hjust=0, vjust=.5, size=fontsize, color="black", parse=TRUE)
+	}
+
 
 	# set scale ticks
-	p1 <- p1 + scale_y_continuous(breaks=c(c(-log(c(100, 30, 10, 3)), 0, log(c(3, 10, 30, 100)))), labels=c("-log(100)", "-log(30)", "-log(10)", "-log(3)", "log(1)", "log(3)", "log(10)", "log(30)", "log(100)"))
+	p1 <- p1 + scale_y_continuous(breaks=y_breaks, labels=c("-log(100)", "-log(30)", "-log(10)", "-log(3)", "log(1)", "log(3)", "log(10)", "log(30)", "log(100)"))
 	
 
 	if (main != "") p1 <- p1 + ggtitle(main)
 
-	#p1 <- p1 + theme(plot.margin = grid::unit(c(1,5,1,1), "lines"))
+	p1 <- p1 + theme(plot.margin = grid::unit(c(1,5,1,1), "lines"))
 
 	# TODO: The annotation only works with this work-around; but now no ggplot-object is returned (which would be nice for users, to add their own themes, e.g.). Set x = Inf
 	# Code to override clipping, from http://stackoverflow.com/questions/10014187/displaying-text-below-the-plot-generated-by-ggplot2 
-	# gt <- ggplot_gtable(ggplot_build(p1))
-	# gt$layout$clip[gt$layout$name == "panel"] <- "off"
-	# grid::grid.draw(gt)
+	gt <- ggplot_gtable(ggplot_build(p1))
+	gt$layout$clip[gt$layout$name == "panel"] <- "off"
+	grid::grid.draw(gt)
 	
-	return(p1)
+	# return(p1)
 }
+
+#seqBFplot(1:3, c(2, 5, 10))
+#seqBFplot(1:4, c(1/4, 0.1, 0.02, 115))
+#seqBFplot(1:3, c(1, 5, 100), fontsize=2)
+
+# set.seed(0xBEEF)
+# x <- cumsum(rnorm(100,0.03,0.3))
+# seqBFplot(10:(length(x)+9), x, log.it=FALSE, ylim=c(1/33, 150))
